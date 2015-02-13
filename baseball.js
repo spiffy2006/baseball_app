a8c871b9450f9e2a4892b89dd5f12c1f1562bbf8
Josh M. Diaz <joshua.michael.diaz@gmail.com>
2015-02-12 21:47:21 -0700

    js added. app is set to a reset state. scoreboard not coded yet.

--- baseball.js
+++ baseball.js
@@ -1,5 +1,39 @@
+function resetBoard() {
+  $('td').each( function() {
+    if(! $(this).hasClass('teamName')) {
+      $(this).html('0');
+    }
+  });
+}
+
+function resetOuts() {
+  $('.out').each( function() {
+    if($(this).hasClass('fa-circle')) {
+      $(this).removeClass('fa-circle');
+    }
+
+    if(! $(this).hasClass('fa-circle-thin')) {
+      $(this).addClass('fa-circle-thin');
+    }
+
+  });
+}
+
+function resetTeamTotal() {
+  $('#team-total').html('0');
+}
+
 $(document).ready( function() {
-    $('#newGame').click( function() {
-        $('#newGamePop').show();
-    });
+  $('#startGame').on('click', function() {
+    var away = $('#awayTeamName').val();
+    var home = $('#homeTeamName').val();
+    $('#main-view').hide();
+    $('#game-view').show();
+    $('#away').html(away);
+    $('#home').html(home);
+    $('#upToBat').html(away);
+    resetBoard();
+    resetOuts();
+    resetTeamTotal();
+  });
 });
